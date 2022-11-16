83ce5e7e53da507d18a402c8f5c0bdcf19672382
Sleyter MONTEIRO <87656234+sleyter92k@users.noreply.github.com>
2022-11-16 04:43:40 +0100

    bdd

---
+++ inc/fonctions.php
@@ -0,0 +1,26 @@
+<?php
+function debug($mavar) {// la fonction avec son paramètre, une variable
+  
+var_dump($mavar);// à cette variable on applique le fonction var_dump()
+
+}
+ // FONCTION POUR EXÉCUTER LES REQUETES PRÉPARÉES
+function executeRequete($requete, $parametres = array()) {  // utile pour toutes les requêtes 1 la requête 2 
+    foreach ($parametres as $indice => $valeur) { // boucle foreach
+        $parametres[$indice] = htmlspecialchars($valeur); // pour éviter les injections SQL
+        global $pdoPF; // * global  "nous permet d'acceder à la variable $pdoPF dans l'espace global du fichier log_bdd.php"
+
+        $resultat = $pdoPF->prepare($requete); //prepare la requete
+        $contact = $resultat->execute($parametres); //et execute
+
+        if ($contact === false ) { 
+            return false; // si la requête n'a pas marché je renvoie "false"
+        } else {
+            return $resultat; // sinon je renvoie les resultats de la requête
+        }// fin if else
+    }// fin foreach
+}// fin fonction
+
+
+
+?>
---
+++ inc/log_bdd.php
@@ -0,0 +1,20 @@
+<?php
+    // 1-Variables de connexion à la BDD
+    $host = 'localhost';// le chemin vers le serveur de données
+    $database = 'a_plague_tale';// le nom de la BDD
+    $user = 'root';// le nom d'utilisateur pour se connecter
+    $psw = 'root';// mdp Database / VIDE POUR PC & root pour MAC
+
+    // Retourne une instance de PDO. La signature de la fonction getInstancePDO implique que l'objet doit être nécessairement de type PDO
+    // Sinon une erreur sera levée
+    $pdoPF = new PDO('mysql:host='.$host.';dbname='.$database,$user,$psw,
+    array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,// pour afficher les warnings SQL dans le navigateur
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',// pour définir le charset des échanges avec la BDD
+    ));
+
+    //4- UNE VARIABLE POUR LE FORMULAIRE
+    $confirmation = '';
+    $erreur = '';
+
+?>
